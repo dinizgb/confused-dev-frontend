69be14d54d51ba51ad35819c93eb9a21a224ff21
Gabriel Diniz <gabrieldiniz@Gabriels-MacBook-Air.local>
2025-05-29 12:33:42 -0300

    Feat: Adding Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center">
       <h2 className="text-2xl font-bold text-gray-800">This is my title!</h2>
       <p className="text-gray-600 mt-3">Heey, description</p>
-      <button className="bg-blue-50 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
-        Learn More
-      </button>
+      <Button>Click me</Button>
     </div>
   )
 }
